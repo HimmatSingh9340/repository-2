0a80f3e0a26e800e285d104381a0b3775442c9b2
HimmatSingh9340 <sin14257@sheridancollege.ca>
2025-01-24 18:17:41 -0500

    value a added and printed

---
+++ src/practiceweek2/Practiceweek2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practiceweek2;
+
+/**
+ *
+ * @author Himmat singh
+ */
+public class Practiceweek2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a= 4;
+        System.out.println(a);
+        // TODO code application logic here
+    }
+    
+}
