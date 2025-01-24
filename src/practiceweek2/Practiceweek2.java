bf9a35c87b2ecdc976360b80f7de1db498fd18fc
HimmatSingh9340 <sin14257@sheridancollege.ca>
2025-01-24 18:25:12 -0500

    deleted bye comment

--- src/practiceweek2/Practiceweek2.java
+++ src/practiceweek2/Practiceweek2.java
@@ -16,8 +16,6 @@ public class Practiceweek2 {
     public static void main(String[] args) {
         int a= 4;
         System.out.println(a);
-//        hey
-//          bye man!
         // TODO code application logic here
     }
     
